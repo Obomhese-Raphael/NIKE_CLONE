e866d348b828e7c91bda27f4ab5dce857a395329
Obomhese Raphael <obomheser@gmail.com>
2024-07-16 17:58:12 +0100

    First Commit

---
+++ nike-clone/src/Components/assets/essentials.js
@@ -0,0 +1,69 @@
+import essentialmen_1 from "./men-s-shoes-clothing-accessories (5).jpg"
+import essentialmen_2 from "./men-s-shoes-clothing-accessories (6).jpg"
+import essentialmen_3 from "./men-s-shoes-clothing-accessories (7).jpg"
+import essentialwomen_1 from "./women-s-shoes-clothing-accessories.jpeg"
+import essentialwomen_2 from "./women-s-shoes-clothing-accessories (3).jpeg"
+import essentialwomen_3 from "./women-s-shoes-clothing-accessories (4).jpeg"
+import essentialkids_1 from "./nike-kids-shoes-clothing-and-accessories-nike-com.jpg"
+import essentialkids_2 from "./nike-kids-shoes-clothing-and-accessories-nike-com (1).jpg"
+
+let essential = [
+    {
+        id: 1,
+        category: "mens",
+        name: "Clothing",
+        image: essentialmen_1,
+    },
+    {
+        id: 2,
+        category: "mens",
+        name: "Shoes",
+        image: essentialmen_2,
+        heading: null
+    },
+    {
+        id: 3,
+        category: "mens",
+        name: "Accessories",
+        image: essentialmen_3,
+        heading: null
+    },
+    {
+        id: 4,
+        category: "womens",
+        name: "Clothing",
+        image: essentialwomen_1,
+        heading: null
+    },
+    {
+        id: 5,
+        category: "womens",
+        name: "Shoes",
+        image: essentialwomen_2,
+        heading: null
+    },
+    {
+        id: 6,
+        category: "womens",
+        name: "Accessories",
+        image: essentialwomen_3,
+        heading: null
+    },
+    {
+        id: 7,
+        category: "kids",
+        name: "",
+        image: essentialkids_1,
+        heading: null
+    },
+    {
+        id: 6,
+        category: "kids",
+        name: "",
+        image: essentialkids_2,
+        heading: null
+    },
+]
+
+
+export default essential
